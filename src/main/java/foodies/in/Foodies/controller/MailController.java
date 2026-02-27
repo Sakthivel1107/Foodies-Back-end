b715369647f3531d34e054068c758392310d01f5
Sakthi vel <svel62628@gmail.com>
2026-02-27 09:18:44 -0800

    mail commit

--- src/main/java/foodies/in/Foodies/controller/MailController.java
+++ src/main/java/foodies/in/Foodies/controller/MailController.java
@@ -5,10 +5,7 @@ import foodies.in.Foodies.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
@@ -16,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MailController {
     @Autowired
     private EmailService emailService;
+    @PostMapping
     public ResponseEntity<String> sendMessage(@RequestBody ContactRequest contactRequest){
         try{
             emailService.sendEmail(contactRequest);
